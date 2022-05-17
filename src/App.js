043a9df62d38f693af96ecd5b27d568934776fb5
mateodelcamp <mateodelcampillo01@gmail.com>
2022-05-17 20:28:39 -0300

    Desafio clase 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import NavBar from './components/NavBar/NavBar.js'
 import './App.css';
 
 function App() {
-  return(
-    <div>
+  return (
+    <div className="NavBar">
       <NavBar />
     </div>
   )
-  }
+}
 
 export default App;
-  
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,30 @@
-const NavBar = () =>{
-return(
-    <div>
-        NavBar
-    </div>
-)
+import './NavBar.css'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import logo from './rick.png'
+
+const NavBar = () => {
+  return (
+
+    <AppBar position="static">
+      <Toolbar>
+        <img src={logo} className="rickLogo" />
+        <h2 className='nombre'>Wubaluba</h2>
+        <ul className='lista'>
+        <span><button>Inicio</button></span>
+        <span><button>Productos</button></span>
+        <span><button>Sobre Nosotros</button></span>
+        </ul>
+        <Button color="inherit">Login</Button>
+        
+
+      </Toolbar>
+    </AppBar>
+
+  )
 }
 export default NavBar
